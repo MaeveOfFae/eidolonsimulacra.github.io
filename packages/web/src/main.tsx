e6a465823926ff798c34bb1d9e2fb61168516336
MaeveOfFae <deadly.drow@gmail.com>
2026-03-11 06:52:27 -0600

    fix: Replace BrowserRouter with HashRouter for improved routing compatibility

--- packages/web/src/main.tsx
+++ packages/web/src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import App from './App';
 import { ThemeProvider } from './components/common/ThemeProvider';
@@ -19,9 +19,9 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <ThemeProvider>
-        <BrowserRouter>
+        <HashRouter>
           <App />
-        </BrowserRouter>
+        </HashRouter>
       </ThemeProvider>
     </QueryClientProvider>
   </StrictMode>
